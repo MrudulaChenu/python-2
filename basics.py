ee90a6cf180accd336c0ac2902b3d350ad2bee97
MrudulaChenu <mrudulachenu@gmail.com>
2026-06-10 00:01:57 +0530

    Add introductory content to basics.py

---
+++ basics.py
@@ -0,0 +1,15 @@
+day-1:
+python was introduced by "GUDIO VAN ROSSUM" in 1991
+why python?
+    Interpreted language.
+    It is high level programming language.
+     Easy to learn.
+     Easy to test and deploy.
+      
+ what is compiler?
+      It  converts high level language into machine code.
+ what is interpreter?
+      execute line-by-line.
+  *variables are used to store the vaiues.
+      a=10
+      name="mad"
